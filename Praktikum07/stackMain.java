a6dc55a94186b11941995f53fda178ad37f40015
Gilang Andhika <user.email>
2024-04-25 20:11:53 +0700

    pertemuan 9 stack

---
+++ Praktikum07/stackMain.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class stackMain {
+    public static void main(String[] args) {
+        stack stk = new stack(5);
+        Scanner sc = new Scanner(System.in);
+
+        // char pilih;
+        // do {
+        //     System.out.print("Jenis: ");
+        //     String jenis = sc.nextLine();
+        //     System.out.print("Warna: ");
+        //     String warna = sc.nextLine();
+        //     System.out.print("Merk: ");
+        //     String merk = sc.nextLine();
+        //     System.out.print("Ukuran: ");
+        //     String ukuran = sc.nextLine();
+        //     System.out.print("Harga: ");
+        //     double harga = sc.nextDouble();
+
+        //     pakaian p = new pakaian(jenis, warna, merk, ukuran, harga);
+        //     System.out.print("Apakah Anda akan menambahkan data baru ke stack (y/n)? ");
+        //     pilih = sc.next().charAt(0);
+        //     sc.nextLine();
+        //     stk.push(p);
+        // } while (pilih == 'y');
+
+        // stk.print();
+        // stk.pop();
+        // stk.peek();
+        // stk.print();
+        boolean menu = true;
+        while (menu) {
+            System.out.println("\nMenu: ");
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Print");
+            System.out.println("5. Exit");
+            System.out.print("Input: ");
+            String choose = sc.nextLine();
+
+        switch (choose) {
+            case "push":
+                System.out.print("\nJenis: ");
+                String jenis = sc.nextLine();
+                System.out.print("Warna: ");
+                String warna = sc.nextLine();
+                System.out.print("Merk: ");
+                String merk = sc.nextLine();
+                System.out.print("Ukuran: ");
+                String ukuran = sc.nextLine();
+                System.out.print("Harga: ");
+                double harga = sc.nextDouble();
+                sc.nextLine();
+
+                pakaian p = new pakaian(jenis, warna, merk, ukuran, harga);
+                stk.push(p);
+                break;
+
+            case "print":
+                stk.print();
+                break;
+            
+            case "pop":
+                stk.pop();
+                break;
+
+            case "peek":
+                stk.peek();
+                break;
+
+            case "exit":
+                menu = false;
+                break;
+        
+            default:
+                System.out.println("Inputmu salah blok");
+                break;
+        }
+        }
+        sc.close();
+
+        
+    }
+}
